1994b9e104895733188f0732b7c09bae30d01935
Cao Thanh Trung <caothanhtrung4193@gmail.com>
2019-11-01 23:27:21 +0700

    create generic base repository.

---
+++ CSharpCafeApp/CSharpCafeApp.Core/Repositories/BaseRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCafeApp.Core.Repositories
+{
+    public class BaseRepository<TEnitity> : IBaseRepository<TEnitity> where TEnitity : class
+    {
+        protected readonly DbSet<TEnitity> _entities;
+
+        public BaseRepository(DbContext context)
+        {
+            _entities = context.Set<TEnitity>();
+        }
+
+        public async Task AddAsync(TEnitity entity)
+        {
+            await _entities.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<TEnitity> entities)
+        {
+            await _entities.AddRangeAsync(entities);
+        }
+
+        public async Task<IEnumerable<TEnitity>> FindAsync(Expression<Func<TEnitity, bool>> predicate)
+        {
+            return await _entities.Where(predicate).ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEnitity>> GetAllAsync()
+        {
+            return await _entities.ToListAsync();
+        }
+
+        public void Remove(TEnitity entity)
+        {
+            _entities.Remove(entity);
+        }
+
+        /// <summary>
+        /// Still working with remove range as simple not async.
+        /// </summary>
+        /// <param name="entities"></param>
+        public void RemoveRange(IEnumerable<TEnitity> entities)
+        {
+            _entities.RemoveRange(entities);
+        }
+
+        public async Task<TEnitity> SingleAsync(Expression<Func<TEnitity, bool>> predicate)
+        {
+            return await _entities.SingleOrDefaultAsync(predicate);
+        }
+    }
+}
---
+++ CSharpCafeApp/CSharpCafeApp.Core/Repositories/IBaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCafeApp.Core.Repositories
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> SingleAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task AddAsync(TEntity entity);
+
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
